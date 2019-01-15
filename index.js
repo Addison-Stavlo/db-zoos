0a3dd9c49dd44bf097f61a65985075ecd297ff09
Addison-Stavlo <AddieStavlo@gmail.com>
2019-01-15 16:35:44 -0500

    CRUD operations functional

--- index.js
+++ index.js
@@ -20,7 +20,17 @@ function checkBody(req,res,next) {
     res.status(400).json({message: 'please provide a name field'})
   }
 }
-
+function checkID(req,res,next) {
+  db('zoos').where({id: req.params.id})
+  .then(zoo => {
+    if(zoo.length){
+      next();
+    }
+    else{
+      res.status(404).json({message: `zoo of id ${req.params.id} not found...`})
+    }
+  })
+}
 // endpoints here
 server.get('/', (req, res) => {
   res.send('api working...')
@@ -64,6 +74,30 @@ server.post('/api/zoos', checkBody, (req, res) => {
       }    
     })
 });
+
+server.put('/api/zoos/:id', checkBody, checkID, (req,res) => {
+  db('zoos').where({id: req.params.id}).update(req.body)
+    .then(count => {
+      db('zoos').where({id: req.params.id})
+        .then(zoo => res.status(200).json({message: `successfully updated item`, item: zoo}))
+    })
+    .catch(err => {
+      if(err.errno == 19){
+        res.status(500).json({message: `name already used`})
+      }
+      else {
+        res.status(500).json(err) 
+      }    
+    })
+})
+
+server.delete('/api/zoos/:id', checkID, (req,res) => {
+  db('zoos').where({id: req.params.id}).del()
+    .then(count => {
+      res.status(200).json({message: `successfully deleted zoo of id ${req.params.id}`})
+    })
+    .catch(err => res.status(500).json(err) )
+})
 
 const port = 3300;
 server.listen(port, function() {
