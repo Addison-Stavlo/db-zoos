d1eea3da440f86db33120392463ea4c7b9b4cdd2
Addison-Stavlo <AddieStavlo@gmail.com>
2019-01-15 17:19:28 -0500

    refactored zoos routes for error handler function

--- zoos/zoos.js
+++ zoos/zoos.js
@@ -1,5 +1,4 @@
 const express = require('express');
-
 const router = express.Router();
 
 const knex = require('knex');
@@ -15,6 +14,7 @@ function checkBody(req,res,next) {
       res.status(400).json({message: 'please provide a name field'})
     }
   }
+
   function checkID(req,res,next) {
     db('zoos').where({id: req.params.id})
     .then(zoo => {
@@ -28,15 +28,15 @@ function checkBody(req,res,next) {
   }
 
   //endpoints
-  router.get('/', (req, res) => {
+  router.get('/', (req, res, next) => {
     db('zoos')
       .then(zoos => {
         res.status(200).json(zoos)
       })
-      .catch(err => res.status(500).json(err));
+      .catch(err => next(err));
   });
   
-  router.get('/:id', (req, res) => {
+  router.get('/:id', (req, res, next) => {
     db('zoos').where({id: req.params.id})
       .then(zoo => {
         if(zoo.length){
@@ -46,10 +46,10 @@ function checkBody(req,res,next) {
           res.status(404).json({message: 'zoo not found...'})
         }
       })
-      .catch(err=>res.status(500).json(err))
+      .catch(err => next(err));
   });
   
-  router.post('/', checkBody, (req, res) => {
+  router.post('/', checkBody, (req, res, next) => {
     db('zoos').insert(req.body)
       .then(ids => {
         db('zoos').where({id: ids[0]})
@@ -57,38 +57,34 @@ function checkBody(req,res,next) {
             res.status(201).json({message: `item added successfully`, item: zoo})
           })
       })
-      .catch(err => {
-        if(err.errno == 19){
-          res.status(500).json({message: `name already used`})
-        }
-        else {
-          res.status(500).json(err) 
-        }    
-      })
+      .catch(err => next(err))
   });
   
-  router.put('/:id', checkBody, checkID, (req,res) => {
+  router.put('/:id', checkBody, checkID, (req,res,next) => {
     db('zoos').where({id: req.params.id}).update(req.body)
       .then(count => {
         db('zoos').where({id: req.params.id})
           .then(zoo => res.status(200).json({message: `successfully updated item`, item: zoo}))
       })
-      .catch(err => {
-        if(err.errno == 19){
-          res.status(500).json({message: `name already used`})
-        }
-        else {
-          res.status(500).json(err) 
-        }    
-      })
+      .catch(err => next(err))
   })
   
-  router.delete('/:id', checkID, (req,res) => {
+  router.delete('/:id', checkID, (req,res, next) => {
     db('zoos').where({id: req.params.id}).del()
       .then(count => {
         res.status(200).json({message: `successfully deleted zoo of id ${req.params.id}`})
       })
-      .catch(err => res.status(500).json(err) )
+      .catch(err => next(err));
   })
+
+  //error handler
+  router.use( (err,req,res,next) => {
+    if(err.errno == 19){
+        res.status(500).json({message: `name already used`})
+      }
+      else {
+        res.status(500).json(err) 
+      }    
+  });
 
   module.exports = router;
